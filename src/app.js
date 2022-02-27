47056ee558d28e91d8016b1c5df843450c722bf1
Zamir Kanji <zamirkanji@comcast.net>
2022-02-27 12:42:22 -0800

    loader, return obj, just need to figure out units from response

--- src/app.js
+++ src/app.js
@@ -3,8 +3,13 @@ import getData from "./getData";
 const getInputValue = () => {
     const form = document.querySelector('#form');
     const input = document.querySelector('#city-input');
+    const mainBody = document.querySelector('#main');
 
     form.addEventListener('submit', async (e) => {
+        if(mainBody.classList.contains('main')) {
+            mainBody.classList.remove('main');
+            mainBody.classList.add('display');
+        }
         e.preventDefault();
         let v = input.value;
         Data(v);
@@ -14,7 +19,6 @@ const getInputValue = () => {
 
 const displayLoading = () => {
     const loader = document.querySelector('#loader');
-    console.log(loader);
     loader.classList.remove('display');
     loader.classList.add('ripple');
 }
@@ -25,6 +29,7 @@ const hideLoading = () => {
     loader.classList.remove('ripple');
     loader.classList.add('display');
     mainBody.classList.remove('display');
+    mainBody.classList.add('main');
 }
 
 // const timeOut = (ms) => {
@@ -34,33 +39,49 @@ const hideLoading = () => {
 const timeOut = () => {
     const timeOut = setTimeout((displayLoading) => {
        hideLoading();
-    },1500)
+    },1000)
     return timeOut
 }
 
 const clear = () => clearTimeout(timeOut);
 
-const Data = async (v) => {
-    let t;
-    let c = getData(v).then(data => {
-        return data;
-    }).then(data => {
-        t = timeOut(displayLoading());
+// const Data = async (v) => {
+//     let t;
+//     let c = getData(v).then(data => {
+//         return data;
+//     }).then(data => {
+//         t = timeOut(displayLoading());
         
-        console.log(data); 
-        return data;
-    }).then(data => {
+//         console.log(data); 
+//         return data;
+//     }).then(data => {
+//         clear(t);
+//         const {main, weather, name, wind} = data;
+//         console.log(main, weather);
+//             getName(name);
+//             getTemp(main.temp);
+//             getWind(wind.speed);
+//             return {data};
+//     }) 
+//     .catch(c => {
+//         console.log(c);
+//     });
+    
+// }
+
+
+const Data = async (v) => {
+    try {
+        let c = await getData(v);
+        const t = timeOut(displayLoading());
+        getName(c.name);
+        getTemp(c.main.temp);
+        getWind(c.wind.speed);
         clear(t);
-        const {main, weather, name, wind} = data;
-        console.log(main, weather);
-            getName(name);
-            getTemp(main.temp);
-            getWind(wind.speed);
-            return {data};
-    }) 
-    .catch(c => {
-        console.log(c);
-    });
+        return c;
+    }catch (err) {
+        console.log(err);
+    }
     
 }
 
--- src/getData.js
+++ src/getData.js
@@ -2,12 +2,19 @@ import { displayLoading, hideLoading } from "./app";
 
 const getData = async (city) => {
     // displayLoading();
+    let DATA;
     try {
         let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=7706c3d5881905dc71d41abe6fc11974`, {mode: 'cors'});
         if(!response.ok) {
             throw new Error(response.status);
-        } 
-        return await response.json();
+        } else {
+            console.log(response);
+            DATA = await response.json();
+            const {weather, main, wind, name} = DATA;
+            let weatherData = {weather, main, wind, name};
+            return weatherData;
+        }
+
     } catch (err) {
         console.log('rejected: ', err);
     }
